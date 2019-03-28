1cd75f153f50f719bec61d01c392719d18a430ef
Main Khan <mukhan@umass.edu>
2019-03-27 23:15:12 -0400

    simplify backend to one file

--- backend/src/routes/routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const LOGGER = require('log4js').getLogger();
-const userController = require('../controller/userController');
-
-router.post('/createUser', userController.createUser);
-
-module.exports = router;
